902101118e880106c2938c5bf3e453c3f5f5b574
lakithjayalath <lakith54321@gmail.com>
2021-09-04 13:56:34 +0530

    Initial Commit

---
+++ Objective-C-Intro/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C-Intro
+//
+//  Created by Lakith Jayalath on 2021-09-04.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
