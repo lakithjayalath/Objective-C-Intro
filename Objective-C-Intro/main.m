4890bda748e7b755d3355d3168a1dc85068fa04b
lakithjayalath <lakith54321@gmail.com>
2021-09-06 10:26:26 +0530

    Functions and NSString

--- Objective-C-Intro/main.m
+++ Objective-C-Intro/main.m
@@ -7,10 +7,27 @@
 
 #import <Foundation/Foundation.h>
 
+int add(int num1, int num2) {
+    return num1 + num2;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        // %i is a format specifier
+        int result = add(4, 20); // int is on the Stack
+        NSLog(@"The result of addition is %i, %i.", result, result);
+        
+        // NSInteger - wrapper around integers and long
+//        NSInteger number = 0;
+        
+        // declare a string
+        // NSString is an object that allocates
+        // memory on the Heap (a place (buffer) in memory)
+        NSString *name = @"Lakith";
+        // swift - let name = "Lakith"
     }
     return 0;
 }
